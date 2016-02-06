2db9c180d35343d954fa067b3c5d9dd58dc4ab61
BoyWeerapon <vrloso08@gmail.com>
2016-02-06 15:05:16 +0700

    Finish_59-2-6

--- app/src/main/java/onzonsoft/weeraphon/boy/boyrestaurant/MainActivity.java
+++ app/src/main/java/onzonsoft/weeraphon/boy/boyrestaurant/MainActivity.java
@@ -1,8 +1,12 @@
 package onzonsoft.weeraphon.boy.boyrestaurant;
 
 import android.database.sqlite.SQLiteDatabase;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
     private MyManage objMyManage;
@@ -22,8 +26,44 @@ public class MainActivity extends AppCompatActivity {
         //Clean data
         cleanData();
 
+        //Syn JSON to SQLite
+        synJSONtoSQLite();
+
 
     }   //Main Method
+
+    private void synJSONtoSQLite() {
+
+        //เปลี่ยน Policy ให้เชื่อมต่อได้(เพราะเป็นนโยบายของ Google)
+        StrictMode.ThreadPolicy myThreadPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(myThreadPolicy);
+
+        int intTable = 1;
+        String tag = "Restaurant";
+
+        while (intTable<=2) {
+
+            //1. การสร้าง Input Stream(การโหลดไป ประมวลผลไป)
+            InputStream objInputStream = null;
+            String strURLuser = "http://swiftcodingthai.com/6feb/php_get_data_boy.php";
+            String strURLfood = "http://swiftcodingthai.com/6feb/php_get_data_food.php";
+
+            try {
+
+            } catch (Exception e) {
+                Log.d(tag, "InputStream ==>" + e.toString());
+            }
+
+            //2. ก่ารสร้าง JSON String(เอาส่งที่โหลดมาเปลี่ยนเป็นสตริง)
+
+            //3. การ Update SQLite(เอาสตริงที่โหลดมาเปลี่ยนเป็น Slqite)
+
+            intTable += 1;
+
+        }   //while
+
+    }   //Syn JSON to SQLite
 
     private void cleanData() {
         SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
