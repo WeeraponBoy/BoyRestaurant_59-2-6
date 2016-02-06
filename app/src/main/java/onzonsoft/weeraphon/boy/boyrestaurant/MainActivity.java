f88b5348ac64c86a22943a707086ad1727154859
BoyWeerapon <vrloso08@gmail.com>
2016-02-06 13:33:24 +0700

    Delete SQLite ได้แล้ว

--- app/src/main/java/onzonsoft/weeraphon/boy/boyrestaurant/MainActivity.java
+++ app/src/main/java/onzonsoft/weeraphon/boy/boyrestaurant/MainActivity.java
@@ -1,5 +1,6 @@
 package onzonsoft.weeraphon.boy.boyrestaurant;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -15,6 +16,28 @@ public class MainActivity extends AppCompatActivity {
         //Request Database
         objMyManage = new MyManage(this);
 
+        //ทดสอบเพิ่มข้อมูล
+       // testAddValue();
+
+        //Clean data
+        cleanData();
+
+
     }   //Main Method
+
+    private void cleanData() {
+        SQLiteDatabase objSqLiteDatabase = openOrCreateDatabase(MyOpenHelper.database_name,
+                MODE_PRIVATE,null);
+        objSqLiteDatabase.delete(MyManage.food_Table, null, null);
+        objSqLiteDatabase.delete(MyManage.user_Table, null, null);
+    }   //Clean data
+
+    private void testAddValue() {
+
+        for (int i = 0; i <= 1; i++) {
+            objMyManage.addNewValue(i, "test1", "test2", "test3");
+        }   //for
+
+    }   //ทดสอบเพิ่มข้อมูล
 
 }   //คลาสหลัก
--- app/src/main/java/onzonsoft/weeraphon/boy/boyrestaurant/MyManage.java
+++ app/src/main/java/onzonsoft/weeraphon/boy/boyrestaurant/MyManage.java
@@ -1,5 +1,6 @@
 package onzonsoft.weeraphon.boy.boyrestaurant;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -8,8 +9,20 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by M_Boyd on 6/2/2559.
  */
 public class MyManage {
+    //ประกาศตัวแปร
     private MyOpenHelper objMyOpenHelper;
     private SQLiteDatabase writeSqLiteDatabase, readSqLiteDatabase;
+
+    public static final String user_Table = "userTABLE";
+    public static final String food_Table = "foodTABLE";
+    public static final String column_id = "_id";
+    public static final String column_User = "User";
+    public static final String column_Pass = "Password";
+    public static final String column_Name = "Name";
+    public static final String column_Food = "Food";
+    public static final String column_Price = "Price";
+    public static final String column_Source = "Source";
+
 
     public MyManage(Context context) {
 
@@ -19,6 +32,36 @@ public class MyManage {
         readSqLiteDatabase = objMyOpenHelper.getReadableDatabase();
 
     } //Constructor
+
+    public long addNewValue(int intTable,
+                            String strColumn2,
+                            String strColumn3,
+                            String strColumn4) {
+
+        ContentValues objContentValues = new ContentValues();
+        long longReturn = 0;
+
+        switch (intTable) {
+            case 0:
+                objContentValues.put(column_User,strColumn2);
+                objContentValues.put(column_Pass,strColumn3);
+                objContentValues.put(column_Name,strColumn4);
+                writeSqLiteDatabase.insert(user_Table, null, objContentValues);
+
+                break;
+            case 1:
+                objContentValues.put(column_Food,strColumn2);
+                objContentValues.put(column_Price,strColumn3);
+                objContentValues.put(column_Source,strColumn4);
+                writeSqLiteDatabase.insert(food_Table, null, objContentValues);
+
+                break;
+        }   //Switch
+
+
+        return longReturn;
+    }
+
 
 }   //คลาสหลัก
 
